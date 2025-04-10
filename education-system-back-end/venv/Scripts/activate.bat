dbd2f8b4a08f9dfac50cf084abec73725352b5bb
Ziqi Pei <pzqfranz@gmail.com>
2025-04-10 20:52:23 +1000

    add mysql cloud

--- education-system-back-end/venv/Scripts/activate.bat
+++ education-system-back-end/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\tanxi\Desktop\education-system-back-end\venv
+set VIRTUAL_ENV=D:\Monash\FIT5120\education-system-back-end\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
