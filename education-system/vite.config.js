3d5be9ad326a9eb530482564ed1f557788d5acf5
Ziqi Pei <pzqfranz@gmail.com>
2025-04-11 02:11:20 +1000

    modify the api config

--- education-system/vite.config.js
+++ education-system/vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000/',
+        target: 'http://3.106.223.97:5001/',
         changeOrigin: true
       }
     }
