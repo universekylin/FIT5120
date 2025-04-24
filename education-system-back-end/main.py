774f0780df295187ffd7cb9f049c6bb686b61543
Ziqi Pei <pzqfranz@gmail.com>
2025-04-24 23:59:56 +1000

    page give cource code

--- education-system-back-end/main.py
+++ education-system-back-end/main.py
@@ -13,6 +13,9 @@ from sqlalchemy import or_, func
 
 app = Flask(__name__)
 CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost:3306/db_education'
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:8uTrTaSJ%26y4gu96cj9LY7%5E@52.63.208.7:3306/mysql_test'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:wdy531520@localhost:3306/db_education'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:123456@localhost:3306/db_education2'
