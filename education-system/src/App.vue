a4578ac84cbf564d65c846826152bfa9f6388ca9
unknown <yzha0953@student.monash.edu>
2025-04-27 17:07:56 +1000

    Resolve merge conflict in App.vue

--- education-system/src/App.vue
+++ education-system/src/App.vue
@@ -1,19 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import ChatBot from './components/ChatBot.vue';
-<<<<<<< HEAD
 import FullProtect from './components/FullProtect.vue'
-=======
->>>>>>> f77a37e2129e813b12e2b1ca0a66dbedd7d46311
 </script>
 
 <template>
   <RouterView />
   <ChatBot/>
-<<<<<<< HEAD
   <FullProtect />
-=======
->>>>>>> f77a37e2129e813b12e2b1ca0a66dbedd7d46311
-
 </template>
 
